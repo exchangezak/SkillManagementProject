48469faf44ea06bbcdea1a6d18a1002142997aec
pgawebdev <pg.archen.webdev@gmail.com>
2020-06-02 15:14:45 +0200

    2ndpush

---
+++ class/ApiUser.php
@@ -0,0 +1,40 @@
+<?php
+
+class ApiUser
+{
+
+    static $confirmation    ="";
+    static $cleApi          ="";
+
+    static function login()
+    {
+        $userForm      = $_REQUEST["username"] ?? "";
+        $passwordForm   = $_REQUEST["password"] ?? "";
+
+        $tabResult = Model::read("user", "pwd", $userForm);
+
+        $passwordHash = password_hash($passwordForm, PASSWORD_DEFAULT);
+
+        foreach ($tabResult as $tabLigne) {
+            extract($tabLigne);
+        }
+
+        if (!empty($tabLigne)) {
+            if (password_verify($passwordForm, $pwd)) 
+            {
+                ApiUser::$confirmation = " Bienvenue $login";
+
+            } 
+            
+            else 
+            {
+                ApiUser::$confirmation =  "Déso pas déso, mauvais mdp ($passwordHash) ! ";
+            }
+        } 
+        
+        else 
+        {
+            ApiUser::$confirmation = "Email non trouvé ($passwordHash)";
+        }
+    }
+}
