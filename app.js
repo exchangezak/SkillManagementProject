401f23ff427a2e8e3736a310975c7291b587f5bd
pgawebdev <pg.archen.webdev@gmail.com>
2020-06-09 15:39:08 +0200

    YYYIKES

--- app.js
+++ app.js
@@ -43,10 +43,13 @@ var cookie = document.cookie;
         load.checkPage= function(){
                 var user = getCookie("User");
                 var activPage = document.location;
-               if(activPage == "http://localhost/projectSkill/SkillManagementProject/index.php" && user == ""){
-                   document.location.replace("login.php")
-               
-           }
+                console.log(activPage);
+                if (typeof user == undefined || user == "" ){
+                       
+                                document.location.replace("login.php")
+                                
+                }        
+                       
         }
         /* FIN Code permettant la redirection si un utilisateur essaie d'accéder directement à la page sans se loger */
 
